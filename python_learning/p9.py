20c243d29f28dbcfe3b0843dabdfc3e2454976e8
SAHANA-UM <samw21398@gmail.com>
2024-08-21 14:59:03 +0530

    change of p9

--- python_learning/p9.py
+++ python_learning/p9.py
@@ -1,6 +1,8 @@
 #Program to Print Math table of a number
 
-num = int(input("Enter a number: "))
+input_num = int (input('Enter a number to print its Math table: '))
 
-for i in range(1,11):
-    print(num'*'i ''='' pro,num * i 
+for i in range(1,16):
+    #print(input_num,'*',i,'=',input_number*i)
+    print('%02d * %02d = %03d' %(input_num,i,input_num*i))
+   
