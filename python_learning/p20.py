6db19264633a0d9923788dfe2a784de0bc22ebb0
SAHANA-UM <samw21398@gmail.com>
2024-08-22 14:14:29 +0530

    change of p20

--- python_learning/p20.py
+++ python_learning/p20.py
@@ -8,7 +8,11 @@ Program to print the Equi Lateral Triangle of N lines
 '''
 
 num_rows = int(input('Enter the number of rows of right angled triangle: '))
-for i in range(0,num_rows):
-    for j in range(0,i+1):
-        print('*', end=' ')
+k = 2 * num_rows + 2
+for i in range(1,num_rows+1):
+    for j in range(0,k):
+        print(end=' ')
+    k = k - 1
+    for j in range(0, i ):
+        print('*',end=' ')
     print()
